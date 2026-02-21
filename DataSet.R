24f884b880a9f854a4bfda1bbb68d7264db07b17
LukeBoehler <luke.boehler@stud.uni-heidelberg.de>
2026-02-21 18:20:53 +0100

    Testing whether our Tree is good or not

--- DataSet.R
+++ DataSet.R
@@ -1,7 +1,9 @@
 # This is a kind of storage room for possible data records and links
 #install.packages("AmesHousing")
 library(AmesHousing)
+library(dplyr)
 library(tidyverse)
 ames<- make_ames()
 glimpse(ames)
-
+head(ames) |> select("Gr_Liv_Area", "Garage_Cars", "Overall_Qual", "Year_Built", "Garage_Area")
+ames$Garage_Cars
